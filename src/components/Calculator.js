22eb86255f762704967bc162b2f799c6aaea80ad
olipliche <olipliche@gmail.com>
2021-09-25 10:27:13 +0100

    Update components calculator file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,153 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: '',
-      next: '',
-    };
-  }
+const Calculator = () => {
+  const [object, setObject] = useState({
+    total: 0,
+    operation: '',
+    next: '',
+  });
 
-  handleClick = (event) => {
-    this.setState((object) => calculate(object, event.target.value));
+  const handleClick = (event) => {
+    setObject(calculate(object, event.target.value));
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="container">
-        <div className="wrapper">
-          <input
-            type="text"
-            className="calculator__output calculator__padding screen"
-            value={` ${
-              total || operation || next
-                ? `${total || ''} ${operation || ''} ${next || ''}`
-                : '0'
-            }`}
-            disabled
-          />
-          <input
-            value="AC"
-            type="button"
-            className="calculator__key calculator__padding btn light-gray"
-            onClick={this.handleClick}
-          />
-          <input
-            value="+/-"
-            type="button"
-            className="calculator__key calculator__key--operator calculator__padding btn light-gray"
-            onClick={this.handleClick}
-          />
-          <input
-            value="%"
-            type="button"
-            className="btn light-gray calculator__key calculator__key--operator calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="÷"
-            type="button"
-            className="btn orange calculator__key calculator__key--operator calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="7"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="8"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="9"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="x"
-            type="button"
-            className="btn orange calculator__key calculator__key--operator calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="4"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="5"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="6"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="-"
-            type="button"
-            className="btn orange calculator__key calculator__key--operator calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="1"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="2"
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            value="3"
-            onClick={this.handleClick}
-          />
-          <input
-            className="btn orange calculator__key calculator__key--operator calculator__padding"
-            value="+"
-            type="button"
-            onClick={this.handleClick}
-          />
-          <input
-            value="0"
-            type="button"
-            className="btn zero light-gray calculator__key calculator__key--zero calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="."
-            type="button"
-            className="btn light-gray calculator__key calculator__padding"
-            onClick={this.handleClick}
-          />
-          <input
-            value="="
-            type="button"
-            className="btn orange calculator__key calculator__key--operator calculator__padding"
-            onClick={this.handleClick}
-          />
-        </div>
+  const { total, operation, next } = object;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <input
+          type="text"
+          className="calculator__output calculator__padding screen"
+          value={` ${
+            total || operation || next
+              ? `${total || ''} ${operation || ''} ${next || ''}`
+              : '0'
+          }`}
+          disabled
+        />
+        <input value="AC" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="+/-" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="%" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="÷" type="button" className="btn orange" onClick={handleClick} />
+        <input value="7" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="8" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="9" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="x" type="button" className="btn orange" onClick={handleClick} />
+        <input value="4" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="5" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="6" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="-" type="button" className="btn orange" onClick={handleClick} />
+        <input value="1" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="2" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="3" type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="+" type="button" className="btn orange" onClick={handleClick} />
+        <input value="0" type="button" className="btn zero light-gray" onClick={handleClick} />
+        <input value="." type="button" className="btn light-gray" onClick={handleClick} />
+        <input value="=" type="button" className="btn orange calculator" onClick={handleClick} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
