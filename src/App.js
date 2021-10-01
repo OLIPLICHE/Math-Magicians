fffc6ef7328e87fca2910c8aea5ea250931bbef6
olipliche <olipliche@gmail.com>
2021-10-01 07:53:01 +0100

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 /* eslint-disable */
 import React from 'react';
-import Calculator from './components/Calculator';
+import Calculator from './pages/Calculator';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,10 +13,15 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Calculator />
-      </div>
-    );
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={Calculator} />
+        <Route exact path="/quotes" component={Quotes} />
+      </Switch>
+    </Router>
+   );
   }
 }
 
